edede88e9af84a5ad8c1210d54626fae026fb1a7
Craig Robbins <craigrobbins@users.sf.net>
2014-06-13 18:48:24 +1000

    Added platimum and restructed some src files

--- geology/init.lua
+++ geology/init.lua
@@ -1,13 +1,9 @@
+-------- Orichalcum
+
 minetest.register_craftitem("mtz:orichalcum_gem", {
     description = "Orichalcum Gem",
     inventory_image = "mtz_geol_gem_orichalcum.png",
 })
-
-minetest.register_craftitem("mtz:ore_titanium", {
-    description = "Titanium Ore",
-    inventory_image = "titanium_titanium.png",
-})
-
 
 minetest.register_node("mtz:stone_with_orichalcum", {
     description = "Orichalchum Ore",
@@ -17,17 +13,6 @@ minetest.register_node("mtz:stone_with_orichalcum", {
     drop = "mtz:orichalcum_gem",
     sounds = default.node_sound_stone_defaults(),
 })
-
-minetest.register_node("mtz:stone_with_titanium", {
-    description = "Titanium Ore (in stone)",
-    tiles = {"default_stone.png^titanium_titanium_in_ground.png"},
-    inventory_image = "default_stone.png^titanium_titanium_in_ground.png",
-    is_ground_content = true,
-    groups = {cracky=1},
-    drop = "mtz:ore_titanium",
-    sounds = default.node_sound_stone_defaults(),
-})
-
 
 minetest.register_ore({
     ore_type       = "scatter",
@@ -53,15 +38,59 @@ minetest.register_ore({
     flags          = "absheight",
 })
 
+-------- Titanium
+
+minetest.register_craftitem("mtz:ore_titanium", {
+    description = "Titanium Ore",
+    inventory_image = "titanium_titanium.png",
+})
+
+minetest.register_node("mtz:stone_with_titanium", {
+    description = "Titanium Ore (in stone)",
+    tiles = {"titanium_titanium_in_ground.png"},
+    is_ground_content = true,
+    groups = {cracky=1},
+    drop = "mtz:ore_titanium",
+    sounds = default.node_sound_stone_defaults(),
+})
+
 minetest.register_ore({
     ore_type       = "scatter",
     ore            = "mtz:stone_with_titanium",
     wherein        = "default:stone",
-    clust_scarcity = 8*8*8,
+    clust_scarcity = 10*10*10,
     clust_num_ores = 10,
     clust_size     = 7,
     height_min     = -31000,
     height_max     = -100,
+    flags          = "absheight",
+})
+
+-------- Platinum
+
+minetest.register_craftitem("mtz:ore_platinum", {
+    description = "Platinum Chunk",
+    inventory_image = "minerals_native_platinum.png",
+})
+
+minetest.register_node("mtz:stone_with_platinum", {
+    description = "Platinum Ore (in stone)",
+    tiles = {"default_stone.png^ores_native_platinum.png"},
+    is_ground_content = true,
+    groups = {cracky=1},
+    drop = "mtz:ore_platinum",
+    sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "mtz:stone_with_platinum",
+    wherein        = "default:stone",
+    clust_scarcity = 18*18*18,
+    clust_num_ores = 8,
+    clust_size     = 4,
+    height_min     = -31000,
+    height_max     = -800,
     flags          = "absheight",
 })
 
