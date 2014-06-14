d88db5e9b4bfa9a78d49f95ae43a59d51d569e6f
Craig Robbins <craigrobbins@users.sf.net>
2014-06-14 19:33:35 +1000

    Added crafting for forged steel tools

--- tools/init.lua
+++ tools/init.lua
@@ -6,6 +6,56 @@ local forgeitems = {
 	{ "default:shovel_steel",  	"mtz:shovel_steel_head", "mtz_steel_shovel_head.png", "Steel shovel head" },
 }
 
+local tooldefs = {
+	{ "mtz:pick_forgedsteel",	"mtz:pick_steel_head" },
+	{ "mtz:shovel_forgedsteel", "mtz:axe_steel_head" },
+	{ "mtz:axe_forgedsteel",	"mtz:axe_steel_head" },
+}
+
+-- FIXME: The inventory image of the crafted tools is wrong, because default is 16x16px and mtz_forged_bg.png is 32x32
+
+local toolattrs = {
+	{
+		description = "Forged Steel Pickaxe",
+		inventory_image = "mtz_forged_bg.png^default_tool_steelpick.png",
+		wield_image = "default_tool_steelpick.png",
+		tool_capabilities = {
+			full_punch_interval = 1.0,
+			max_drop_level=1,
+			groupcaps={
+				cracky = {times={[1]=4.00, [2]=1.60, [3]=0.80}, uses=40, maxlevel=2},
+			},
+			damage_groups = {fleshy=4},
+		},
+	},
+	{
+		description = "Forged Steel Shovel",
+		inventory_image = "mtz_forged_bg.png^default_tool_steelshovel.png",
+		wield_image = "default_tool_steelshovel.png^[transformR90",
+		tool_capabilities = {
+			full_punch_interval = 1.1,
+			max_drop_level=1,
+			groupcaps={
+				crumbly = {times={[1]=1.50, [2]=0.90, [3]=0.40}, uses=60, maxlevel=2},
+			},
+			damage_groups = {fleshy=3},
+		},
+	},
+	{
+		description = "Forged Steel Axe",
+		inventory_image = "mtz_forged_bg.png^default_tool_steelaxe.png",
+		wield_image = "default_tool_steelaxe.png",
+		tool_capabilities = {
+			full_punch_interval = 1.0,
+			max_drop_level=1,
+			groupcaps={
+				choppy={times={[1]=2.50, [2]=1.40, [3]=1.00}, uses=40, maxlevel=2},
+			},
+			damage_groups = {fleshy=4},
+		},
+	}
+}
+
 -- TODO: Make this a function and call the function from within the loop
 
 for i in ipairs(forgeitems) do
@@ -37,4 +87,19 @@ for i in ipairs(forgeitems) do
 		recipe = out_item,
 		cooktime = 10
 	})
+		
+	-- TODO: a) Make a function; b) Try and get the base values from the default game and modify them by a %
+	for t in ipairs(tooldefs) do
+		local name = tooldefs[t][1]
+		local head = tooldefs[t][2]
+		minetest.register_tool(name, toolattrs[t])
+		minetest.register_craft({
+			output = name,
+			recipe = {
+				{'', head, ''},
+				{'', 'group:stick', ''},
+				{'', 'group:stick', ''},
+			}
+		})
+	end
 end
