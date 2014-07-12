80ba3ac324ddad73ecc3090f9b13f3e0869214f6
Craig Robbins <crobbins@localhost.localdomain>
2014-07-12 14:25:52 +1000

    Experimental tree updated

--- tests/init.lua
+++ tests/init.lua
@@ -1,31 +1,29 @@
 local experimental_tree = {
-    --- Trunk, FaceHorizotal, MainLeaderLeader
-    axiom="/ FFF A FFF [&-- // b //// b //// b] F/[D-D][+D+D]",
-    rules_a = "[&&TT &TTT ^^TB&TTT] // [--&&&&D][++&&&&D] // A",
-    rules_b = "[^--TTTT C &&D][^----TTTT C &&D][^++TTTT C &&D][^++++TTTT C &&D]",
-    rules_c = "&&//TTTC",
-    rules_d = "[FF/FF/FF/FF]",
+	axiom="/ FFF[ccc] A FF /// [C^^D^D^D] /// [C^^D^D^D] /// [c^^d^d^d] &&&&& / ///B ///B ///B",
+	rules_a = "[&&TTTTTT[d]TTT[b]&TTT]////[&&TTTTTT[d]TTT[b]&TTT]////[&&TTTTTT[d]TTT[b]&TTT]",
+	rules_b = "[^^^^+++***TT[d]TTb[d]^TTT^F [c]d][^^^^+++///TT[d]TTb[d]^TTT^F [c]d][^^^^++++///TT[d]TTb[d]^TTT^F [c]d]",
+	rules_c = "&&&&FfF [D]",
+	rules_d = "//FFf",
 
-    trunk="default:tree",
-    leaves="default:leaves",
-    --leaves2="",
-    --leaves2_chance = 5,
-    angle=30,
-    iterations=3,
-    random_level=3,
-    trunk_type="crossed",
-    thin_branches=true
+	trunk="default:jungletree",
+	leaves="default:jungleleaves",
+	angle=30,
+	iterations=3,
+	random_level=3,
+	trunk_type="crossed",
+	thin_branches=true
 }
 
 minetest.register_craftitem("mtz:test_treespawner", {
-    description = "Test Experimental Tree",
-    inventory_image = "default_stick.png",
-    on_use = function(itemstack, user, pointed_thing)
-        local hasprivs = minetest.check_player_privs(user:get_player_name(), {tester=true})
+	description = "Test Experimental Tree",
+	inventory_image = "default_stick.png",
+	on_use = function(itemstack, user, pointed_thing)
+		local hasprivs = minetest.check_player_privs(user:get_player_name(), {tester=true})
 
-        if hasprivs == true and pointed_thing.type == "node" then
-            local pos = pointed_thing.above
-            minetest.env:spawn_tree(pos, experimental_tree)
-        end
-    end
+		if hasprivs == true and pointed_thing.type == "node" then
+				local pos = pointed_thing.above
+				experimental_tree.seed = math.random(0, 32767)
+				minetest.env:spawn_tree(pos, experimental_tree)
+		end
+	end
 })
