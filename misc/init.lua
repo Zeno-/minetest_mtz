b39e223b5f1115bf5d856656c060b194f3f9419e
Craig Robbins <craigrobbins@users.sf.net>
2014-08-18 18:52:44 +1000

    removed joke

--- misc/init.lua
+++ misc/init.lua
@@ -11,23 +11,3 @@ minetest.register_craft({
         }
 })
 
-minetest.register_node("mtz:baconandeggs", {
-	description = "Bacon and Eggs",
-	drawtype = "plantlike",
-	tiles = { "bande32.png" },
-	inventory_image = "bande32.png",
-	weild_image = "bande32.png",
-	drop = "mtz:baconandeggs",
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {	snappy=3, flammable=0, flower=0,
-							flora=1, attached_node=1 },
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-			type = "fixed",
-			fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
-	},
-	on_use = minetest.item_eat(16),
-})
