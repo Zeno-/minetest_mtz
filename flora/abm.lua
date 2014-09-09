2170191429593e529f748fd0528df54802ac24d0
Craig Robbins <kde.psych@gmail.com>
2014-09-09 13:37:35 +1000

    Change mushroom spawning method

--- flora/abm.lua
+++ flora/abm.lua
@@ -9,29 +9,32 @@ minetest.register_abm({
 		interval = mycena_interval,
 		chance = mycena_chance,
 		action = function(pos, node)
-				local destnode = { x = pos.x, y = pos.y+1, z = pos.z}
-				local name = minetest.get_node(destnode).name
+			local destnode = { x = pos.x + 3 - math.random(6), y = pos.y+1, z = pos.z + 3 - math.random(6)}
+			local name = minetest.get_node(destnode).name
+			local nameunder = minetest.get_node({x = destnode.x, y = destnode.y-1, z = destnode.z}).name
 
-				if (name ~= "air") then
-						return
-				end
+			if name ~= "air" or (nameunder ~= "default:dirt" and nameunder ~= "default:dirt_with_grass") then
+				return
+			end
 
-				if not minetest.find_node_near(destnode, 2, "default:jungletree") then
+			if not minetest.find_node_near(destnode, 2, "default:jungletree") then
+				return
+			end
+
+			local pos0 = { x = destnode.x-6, y = destnode.y-6, z = destnode.z-6 }
+			local pos1 = { x = destnode.x+6, y = destnode.y+6, z = destnode.z+6 }
+
+			local mushcount = minetest.find_nodes_in_area(pos0, pos1, "mtz_flora:mycena")
+
+			if #mushcount > 3 then
 					return
-				end
+			end
 
-				local pos0 = { x = destnode.x-5, y = destnode.y-5, z = destnode.z-5 }
-				local pos1 = { x = destnode.x+5, y = destnode.y+5, z = destnode.z+5 }
-
-				local mushcount = minetest.find_nodes_in_area(pos0, pos1, "mtz_flora:mycena")
-
-				if #mushcount > 2 then
-						return
-				end
-
-				local nodechoices = { "mtz:mycena", "mtz:mycena_2", "mtz:mycena_3", "mtz:mycena_4" }
-				local nodename = nodechoices[math.random(1, 4)]
-				minetest.set_node(destnode, {name = nodename})
+			local nodechoices = { "mtz:mycena", "mtz:mycena_2", "mtz:mycena_3", "mtz:mycena_4" }
+			local nodename = nodechoices[math.random(1, 4)]
+			-- param2 is random rotation by 0 to 179 in steps of 2 degress. Using range 0 to 82
+			-- and doubling so that the rotation is in steps of 4
+			minetest.set_node(destnode, {name = nodename, param2 = math.random(0, 82)*2})
 		end
 })
 
@@ -42,21 +45,22 @@ minetest.register_abm({
 	chance = 4000,
 
 	action = function(pos, node)
-		local destnode = { x = pos.x, y = pos.y+1, z = pos.z}
+		local destnode = { x = pos.x + 2 - math.random(4), y = pos.y+1, z = pos.z + 2 - math.random(4)}
 		local name = minetest.get_node(destnode).name
+		local nameunder = minetest.get_node({x = destnode.x, y = destnode.y-1, z = destnode.z}).name
 
-		if (name ~= "air") then
+		if name ~= "air" or (nameunder ~= "default:dirt" and nameunder ~= "default:dirt_with_grass") then
 				return
 		end
 
-		local pos0 = { x = destnode.x-4, y = destnode.y-4, z = destnode.z-4 }
-		local pos1 = { x = destnode.x+4, y = destnode.y+4, z = destnode.z+4 }
+		local pos0 = { x = destnode.x-5, y = destnode.y-5, z = destnode.z-5 }
+		local pos1 = { x = destnode.x+5, y = destnode.y+5, z = destnode.z+5 }
 
 		local mushcount = minetest.find_nodes_in_area(pos0, pos1,
 			{ "mtz:mushroom_a", "mtz:mushroom_a_group" }
 		)
 
-		if #mushcount > 3 then
+		if #mushcount > 2 then
 			return
 		end
 
